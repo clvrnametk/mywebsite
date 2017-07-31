fca8bd120a1cd81b92f9bdb0e0feadd95e2445ce
Mike Morales <mike.morales.ny@gmail.com>
2017-07-30 20:55:21 -0400

    Finished prototype for website

--- 05_sass_bourbon_test_3/assets/js/app.js
+++ 05_sass_bourbon_test_3/assets/js/app.js
@@ -1,1 +1,31 @@
+// Check to see if doc is laoded
 console.log('loaded bro!!');
+
+
+//------------------------------------
+// GLOBAL: Event Listeners
+//------------------------------------
+
+// Shrink meny on scroll down
+$(document).on ('scroll', function(){
+  if ($(document).scrollTop() > 100)
+  {
+    $('header').addClass('shrink');
+  }
+  else
+  {
+    $('header').removeClass('shrink');
+  }
+});
+
+// Shift Counter up when the page goes to the bottom
+$(window).scroll(function() {
+   if($(window).scrollTop() + $(window).height() == $(document).height())
+   {
+       $('aside').addClass('shift-up');
+   }
+   else
+   {
+     $('aside').removeClass('shift-up');
+   }
+});
